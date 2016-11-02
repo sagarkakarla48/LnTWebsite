57191c972b8c12ace348c3d43f114223ce8e0da3
nikhitha123 <nikhitha.452@gmail.com>
2016-11-02 12:01:16 +0530

    adding details

--- public/clientcontroller.js
+++ public/clientcontroller.js
@@ -1,12 +1,12 @@
 var app=angular.module("myapp",[]);
-app.controller("mycontroller",["$scope","$http",function($scope,$http){
+app.controller("mycntrl",["$scope","$http",function($scope,$http){
 	var refresh=function(){
-		$http.get("/clientlist").success(function(response){
-			$scope.clientt=response;
-			$scope.contact="";
+		var id='ObjectId("58188c2f300c184257537da2")';
+		$http.get("/clientlist/"+id).success(function(response){
+			$scope.clienttable=response;
+			console.log(response);
 		});
 	}
-	$http.get("/clientlist").success(function(response){
-		console.log(response)
-		$scope.clienttable=response;
-	});
+	refresh();
+}]
+);
--- server.js
+++ server.js
@@ -2,30 +2,26 @@ var express=require("express");
 var app=express();
 var mongoose=require("mongoose");
 var bodyParser=require("body-parser");
-var Feedback=require("./models/feedbackdetails");
+var Contact=require("./models/clientsignup");
+var client=require("./models/client");
 var PORT=process.env.PORT || 3000;
-var path = require('path');
-
-mongoose.connect("mongodb://localhost/lntwebsite",function(){
-	console.log("successfully connected to mogodb");
+var path = require('path');mongoose.connect("mongodb://localhost/lntwebsite",function(){
+	console.log("successfully connected");
 });
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.get("/clientlist/:id",function(req,res){
+ 	var id=req.params.id;
+ 	client.getClientdetails(id,function(err,data){
+ 		if(err){
+ 			throw err;
 
-
-app.post("/feedbackList",function(req,res){
-	
-var body=req.body;//will fetch body details
-
-Feedback.addContact(body,function(err,data){
-	if(err){
-		throw err;
-	}
-		res.json(data);
-})
-})
-
+ 		}
+ 		res.json(data);
+ 	})
+ })
 
 app.listen(PORT,function(){
 	console.log("Server is running in number "+PORT);
